8ce75eadabbb91c92f9b6c2927e5e2d06f31fd1c
mjh9401@naver.com <mjh9401@naver.com>
2024-06-09 00:07:40 +0900

    interface, 에러페이지 생성

---
+++ src/types/note.ts
@@ -0,0 +1,15 @@
+import { Tag } from "./tag";
+
+export interface Note {
+    title : string;
+    content : string;
+    tags : Tag[];
+    color : string;
+    priority : string;
+    isPinned : boolean;
+    isRead : boolean;
+    date : string;
+    createTime : number;
+    editedTiem : null | number;
+    id : string;
+}
